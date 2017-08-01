168bc45dbb2ffc0386973edb749e85aa3dc1bf9a
Aswanth <aswanthkumar25@gmail.com>
2017-08-01 15:53:44 +0530

    used jquery for same functionality

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-myApp = {
+$(document).ready(function() {
   /**
    * check if the the provided email is already registered
    * @param  {string} email new email address
    * @return {boolean}       states of the uniqueness check.
    */
-  checkDuplicateEmail: function(email) {
+  function checkDuplicateEmail(email) {
     if(typeof(Storage)!=="undefined")
     {
       const item = localStorage.getItem(email);
@@ -18,28 +18,28 @@ myApp = {
       alert('No storage available');
       return false;
     }
-  },
+  }
+
   /**
    * To register a user to the system. save the user details in browser localStorage
    * with email as key
    * @return {[type]} [description]
    */
-  register: function() {
-    const name = document.getElementById("reg-name").value;
-    const email = document.getElementById("reg-email").value;
-    const password = document.getElementById("reg-password").value;
-    const cpassword = document.getElementById("reg-cpassword").value;
+
+  $('#triggerRegister').click(function(){
+    const name = $("#reg-name").val();
+    const email = $("#reg-email").val();
+    const password = $("#reg-password").val();
+    const cpassword = $("#reg-cpassword").val();
     if (password != cpassword) {
       alert('Password is not matching');
     } else {
-      if (this.checkDuplicateEmail(email)) {
-        const data = {
-          name: name,
-          password: password,
-        };
+      if (checkDuplicateEmail(email)) {
+        const data = { name: name, password: password };
         localStorage.setItem(email, JSON.stringify(data));
         window.location.href = 'login.html';
       }
     }
-  }
-}
+  });
+
+});
